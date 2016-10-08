e2b8d9411236c43aa5671065564c4fd3f370c8ea
Anavros <jtdobbs1@me.com>
2016-10-08 04:07:39 -0400

    Attempted to fix teapot bounding box.

--- main/java/boozemod/blocks/TileEntityTeapot.java
+++ main/java/boozemod/blocks/TileEntityTeapot.java
@@ -1,7 +1,15 @@
 package boozemod.blocks;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class TileEntityTeapot extends TileEntity{
-
+public class TileEntityTeapot extends TileEntity {
+    @Override
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getRenderBoundingBox() {
+        return new AxisAlignedBB(0.25, 0.25, 0.0, 0.75, 0.75, 1.0);
+    }
 }
